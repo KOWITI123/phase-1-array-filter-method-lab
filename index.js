035ee44857403b959f1cb571f167a38e5fa7c9a9
Allan <allan.kowiti@student.moringaschool.com>
2022-08-18 22:24:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+
+const findMatching = (x, y) => x.filter(x =>x.toLowerCase() === y.toLowerCase());
+
+const fuzzyMatch =  (x, y) => x.filter(x =>x.charAt(0) === y.charAt(0));
+
+const matchName = (x, y) => x.filter(x => x.name === y);
